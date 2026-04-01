bd8274d3a4f6ea613ec2aa3b973cc85a48366e51
Stephen <sfolkart@udel.edu>
2026-04-01 10:49:58 -0400

    Beginning of layout

--- app.py
+++ app.py
@@ -23,3 +23,16 @@ else:
 
 #st.set_page_config(page_title="Smart Coffee Kiosk Application")
 st.title("Health Tracker for ashma *Working Title*")
+
+
+
+# Make dashboard
+
+# Dashboard should include: daily check in, wind power test with peak flow meter,
+# and an character of our choice which shows their percentage of healthiness for the day (based on peak flow meter and daily check in)
+
+
+#We want a home page, journal, awards, and a help section (maybe could implement the chatbot here)
+
+#Journal should include a calendar where the user can write notes on their breathing throughout the day
+
